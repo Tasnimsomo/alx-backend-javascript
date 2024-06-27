b60dc62a52fca470bcb422ed51ed1c42cfd01ecb
root <root@TasnimSomo>
2024-06-27 12:26:08 +0400

    0x02. ES6 classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,12 @@
-// 5-building.js
 export default class Building {
-    constructor(sqft) {
-        this._sqft = sqft;
+  constructor(sqft) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
+  }
 
-    get sqft() {
-        return this._sqft;
-    }
-
-    evacuationWarningMessage() {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+  get sqft() {
+    return this._sqft;
+  }
 }
-
