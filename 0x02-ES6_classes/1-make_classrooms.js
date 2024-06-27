9d10aa76a1e72c4f3adf5cf4e3c96a6216b9d134
root <root@TasnimSomo>
2024-06-27 12:31:43 +0400

    0x02. ES6 classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,9 @@
-// 1-make_classrooms.js
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-    return [
-        new ClassRoom(18),
-        new ClassRoom(20),
-        new ClassRoom(34)
-    ];
+  const class1 = new ClassRoom(19);
+  const class2 = new ClassRoom(20);
+  const class3 = new ClassRoom(34);
+  const newList = [class1, class2, class3];
+  return (newList);
 }
-
