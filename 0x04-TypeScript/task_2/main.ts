af0f964575cb82440a7feae3899c725f8b8c4b12
root <root@TasnimSomo>
2024-07-03 14:19:49 +0400

    0x04. Typescript

---
+++ 0x04-TypeScript/task_2/main.ts
@@ -0,0 +1,41 @@
+interface DirectorInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+	workFromHome() {
+		return "Working from home"
+	}
+	getToWork() {
+		return "Getting a coffee break"
+	}
+	workDirectorTasks() {
+		return "Getting to director tasks"
+	}
+}
+
+class Teacher implements TeacherInterface {
+	workFromHome() {
+		return "Cannot work from home"
+	}
+	getCoffeeBreak() {
+		return "Cannot have a break"
+	}
+	workTeacherTasks() {
+		return "Getting to work"
+	}
+
+function createEmployee(salary: number) {
+	if salary < 500 {
+		return new Teacher
+	else
+		return new Director
+	}
