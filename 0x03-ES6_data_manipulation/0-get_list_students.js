9210e87d18dc4b06ac08a2761c906248f944ef51
root <root@TasnimSomo>
2024-07-19 04:56:06 +0400

    0x03. ES6 data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export function getListStudents() {
+	const array = [{id: 1, firstName: 'Guillaume', location: 'San Fransisco'},
+		{id: 2, firstName: 'James', location: 'Columbia'},
+		{id: 5, firstName: 'Serena', location: 'San Fransisco'}
+]
+	return array
+}
